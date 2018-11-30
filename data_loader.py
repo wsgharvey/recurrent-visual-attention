3550888bdaf8796cdd57d690a56d8a13f0fcf7d1
Will Harvey <williamharvey08@gmail.com>
2018-11-30 11:30:10 -0800

    wip - mixing in data wiith supervision

--- data_loader.py
+++ data_loader.py
@@ -137,7 +137,12 @@ def get_test_loader(data_dir,
     return data_loader
 
 class GenModelDataset(Dataset):
-    def __init__(self, transform, epoch_size, fix_data=False, fix_offset=0):
+    def __init__(self,
+                 transform,
+                 epoch_size,
+                 fix_data,
+                 fix_offset,
+                 supervise_attention_freq):
         """
         transform: preprocessing step for data
         fix_data: if True, will use index as random seed when returning a sample
@@ -146,21 +151,30 @@ class GenModelDataset(Dataset):
         self.epoch_size = epoch_size
         self.fix_data = fix_data
         self.fix_offset = fix_offset
+        self.supervise_attention_freq = supervise_attention_freq
+        self.supervise_indicator = 0
 
     def __getitem__(self, index):
+        self.supervise_indicator += supervise_attention_freq
+        if self.supervise_indicator >= 1:
+            self.supervise_indicator -= 1
+            return self._get_supervised(...)
+        else:
+            return self._generate_unsupervised(index)
+
+    def _generate_unsupervised(self, index):
         if self.fix_data:
             rng_state = torch.get_rng_state()
             torch.manual_seed(index+self.fix_offset)
-
         trace_dict = mnist_model()
-
         if self.fix_data:
             torch.set_rng_state(rng_state)
-
         raw = trace_dict['image'].data.view(1, 28, 28)
         image = self.transform(raw)
         digit_label = trace_dict['label']
-        return image, digit_label
+        attention_target = torch.zeros(10)
+        attention_target_exists = torch.tensor(0.)
+        return image, digit_label, target, target_exists
 
     def __len__(self):
         return self.epoch_size
@@ -170,11 +184,15 @@ def get_gen_model_loader(batch_size,
                          fix_data,
                          fix_offset=0,
                          num_workers=4,
-                         pin_memory=False):
+                         pin_memory=False,
+                         supervise_attention_freq=0):
     """
     imitates data loaders but supplies infinite stream from generative model
 
     fix random seed externally if desired
+
+    supervise_attention_freq (float in range [0, 1]) is how often to give a
+    sample with attention targets
 
     actually very silly
     """
@@ -183,7 +201,8 @@ def get_gen_model_loader(batch_size,
     dataset = GenModelDataset(normalize,
                               epoch_size,
                               fix_data=fix_data,
-                              fix_offset=fix_offset)
+                              fix_offset=fix_offset,
+                              supervise_attention_freq)
 
     sampler = SubsetRandomSampler(range(epoch_size))
 
